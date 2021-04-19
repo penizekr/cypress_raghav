d1f093a75656d4e0d2d0cbb8cbd0ad9773b84793
penizekr <56937342+penizekr@users.noreply.github.com>
2021-04-19 08:44:35 +0200

    Add files via upload

--- cypress/integration/my_firs_test.js
+++ cypress/integration/my_firs_test.js
@@ -1,4 +1,7 @@
 /// ＜reference types="cypress" /＞
 it('Gets, types and asserts', () => {
-  cy.visit('https://google.com')
+  cy.visit('https://www.bing.com')
+  cy.get('#sbi_b') //musi se objevit ikonka pro nahravani hlasu. Pak je mozne zacit psat text do vyhledavace. Jinak se psani prerusi.
+  cy.get('#sb_form_q').type('automation step by step raghav pal')
+ // cy.get('#sb_form_q').type
 })
